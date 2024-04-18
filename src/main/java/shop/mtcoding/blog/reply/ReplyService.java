048adb96cdbf775acedc62c7105cc8f4dfb3b4e1
SCHcoding <codingb007@gmail.com>
2024-04-18 11:29:49 +0900

    댓글 쓰기

--- src/main/java/shop/mtcoding/blog/reply/ReplyService.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyService.java
@@ -1,13 +1,27 @@
 package shop.mtcoding.blog.reply;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import shop.mtcoding.blog._core.errors.exception.Exception404;
+import shop.mtcoding.blog.board.Board;
+import shop.mtcoding.blog.board.BoardJPARepository;
+import shop.mtcoding.blog.user.User;
 
 @RequiredArgsConstructor
 @Service
 public class ReplyService {
     private final ReplyJPARepository replyJPARepository;
+    private final BoardJPARepository boardJPARepository;
 
+    @Transactional
+    public void 댓글쓰기(ReplyRequest.SaveDTO requestDTO, User sessionUser) {
+        Board board = boardJPARepository.findById(requestDTO.getBoardId())
+                        .orElseThrow(() -> new Exception404("없는 게시글에 댓글을 작성할 수 없습니다"));
+
+        replyJPARepository.save(requestDTO.toEntity(sessionUser, board));
+
+    }
 
 
 
